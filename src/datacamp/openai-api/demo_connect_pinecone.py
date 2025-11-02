97cb5e9d8f113205d2fd1864cb075f0c13cae66b
tungnn <tungnn.hn@gmail.com>
2025-11-02 19:12:20 +0700

    feat(pinecone): add demo for Pinecone client initialization

---
+++ src/datacamp/openai-api/demo_connect_pinecone.py
@@ -0,0 +1,5 @@
+# Import the Pinecone library
+from pinecone import Pinecone
+
+# Initialize the Pinecone client
+pc = Pinecone(api_key="pcsk_5DHers_EPLqXMn3WFmvxCJPaoRtKSMu95pHTdDxQhq2oehnsdRUv6Qbwx5rctGnzaebCHv")
